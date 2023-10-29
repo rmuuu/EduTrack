76e8541727f5803336ad44436abc7cc5b2a4128b
Aeldred Dela Cruz <21-07614@g.batstate-u.edu.ph>
2023-10-29 15:49:00 +0800

    change username to email

--- src/authentication.py
+++ src/authentication.py
@@ -1,28 +1,17 @@
-import mysql.connector as mysql
-db = mysql.connect(host="localhost",user="root",password="",database="EduTrack")
-command_handler = db.cursor(buffered=True)
+import database
 
-STUDENT = 'student'
-TEACHER = 'teacher'
+def authenticate_user(email, password):
+    with database.Database() as db:
+        # Define the query parameters
+        query_vals = (email, password)
 
-def authenticate_user(username, password):
-    # Define the query parameters
-    query_vals = (username, password)
+        # Execute the SQL query to check if the user with the provided credentials exists
+        query = "SELECT * FROM Users WHERE email = %s AND password = %s"
+        user_record = db.fetch_one(query, query_vals)
+        print(user_record)
+        if user_record:
+            # Extract the user's email and role from the query result
+            email, role = user_record[1], user_record[3]
+            return email, role
 
-    # Execute the SQL query to check if the user with the provided credentials exists
-    command_handler.execute("SELECT * FROM users WHERE username = %s AND password = %s", query_vals)
-
-    # Fetch the user record
-    user_record = command_handler.fetchone()
-
-    if user_record:
-        # Extract the user's username and role from the query result
-        username, role = user_record[1], user_record[3]
-        return User(username, role)
-
-    return None
-
-class User:
-    def __init__(self, username, role):
-        self.username = username
-        self.role = role
+        return None
