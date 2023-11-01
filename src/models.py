c7136ed47ec1d2a0af4c3f14ea7d0e1e4322f746
Aeldred Dela Cruz <21-07614@g.batstate-u.edu.ph>
2023-11-01 22:14:37 +0800

    cleaning and aligning displays

--- src/models.py
+++ src/models.py
@@ -10,20 +10,6 @@ class Person:
         self.eContact_number = eContact_number
         self.eContact_name = eContact_name
 
-    @staticmethod
-    def collect_person_input():
-        last_name = input("Enter last name   : ")
-        given_name = input("Enter given name  : ")
-        middle_name = input("Enter middle name : ")
-        birth_date = input("Enter birth date (YYYY-MM-DD) : ")
-        email = input("Enter email address : ")
-        address = input("Enter address       : ")
-        contact_number = input("Enter contact number           : ")
-        eContact_number = input("Enter emergency contact number : ")
-        eContact_name = input("Enter emergency contact name   : ")
-
-        return last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name
-
 class Student(Person):
     def __init__(self, sr_code, last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name, program, block):
         super().__init__(last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name)
@@ -33,11 +19,21 @@ class Student(Person):
 
     @staticmethod
     def collect_student_input():
-        sr_code = input("Enter SR Code     : ")
-        program = input("Enter Program       : ")
-        block = input("Enter Block         : ")
-        last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name = Person.collect_person_input()
-
+        print("Please Enter :")
+        sr_code =         input(" Enter SR Code : ")
+        last_name =       input("     Last name : ")
+        given_name =      input("    Given name : ")
+        middle_name =     input("   Middle name : ")
+        birth_date =      input(" Birth date\n  (YYYY-MM-DD) : ")
+        email =           input(" Email address : ")
+        program =         input("       Program : ")
+        block =           input("         Block : ")
+        address =         input("       Address : ")
+        contact_number =  input("Contact number : ")
+        print("Incase of emergency.")
+        eContact_name =   input("  Contact name : ")
+        eContact_number = input("Contact number : ")
+        
         return sr_code, last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name, program, block
 
 class Instructor(Person):
@@ -49,11 +45,21 @@ class Instructor(Person):
 
     @staticmethod
     def collect_instructor_input():
-        emp_code =   input("Enter Employee Code  : ")
-        hdl_course = input("Enter Handled Course : ")
-        hdl_block =  input("Enter Handled Block  : ")
-        last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name = Person.collect_person_input()
-
+        print("Please Enter :")
+        emp_code =        input(" Employee Code : ")
+        last_name =       input("     Last name : ")
+        given_name =      input("    Given name : ")
+        middle_name =     input("   Middle name : ")
+        birth_date =      input(" Birth date\n  (YYYY-MM-DD) : ")
+        email =           input(" Email address : ")
+        hdl_course =      input("Handled Course : ")
+        hdl_block =       input("Handled Block  : ")
+        address =         input("       Address : ")
+        contact_number =  input("Contact number : ")
+        print("Incase of emergency.")
+        eContact_name =   input("  Contact name : ")
+        eContact_number = input("Contact number : ")
+        
         return emp_code, last_name, given_name, middle_name, birth_date, email, address, contact_number, eContact_number, eContact_name, hdl_course, hdl_block
         
 class User:
@@ -64,17 +70,17 @@ class User:
         self.role = role
 
 class Class:
-    def __init__(self, class_id, class_name, teacher_id):
+    def __init__(self, class_id, class_name, instructor_id):
         self.class_id = class_id
         self.class_name = class_name
-        self.teacher_id = teacher_id
+        self.instructor_id = instructor_id
 
     @staticmethod
     def collect_class_input():
         class_name = input("Enter class name : ")
-        teacher_id = input("Enter teacher ID : ")
+        instructor_id = input("Enter instructor ID : ")
 
-        return class_name, teacher_id
+        return class_name, instructor_id
 
 class Attendance:
     def __init__(self, attendance_id, student_id, class_id, date, status):
