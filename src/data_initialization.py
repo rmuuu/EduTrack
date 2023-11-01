1d8b088947cbea0c88422cf19e766d96436df4f6
Aeldred Dela Cruz <21-07614@g.batstate-u.edu.ph>
2023-11-01 22:11:35 +0800

    clean, and make almost all generic

--- src/data_initialization.py
+++ src/data_initialization.py
@@ -1,47 +1,34 @@
 import models
 import database
 import authentication
+import user_interface
 from prettytable import PrettyTable
-def add_student():
-    student_info = models.Student.collect_student_input()
-    new_student = models.Student(*student_info)
-    
-    query = "INSERT INTO Students (sr_code, last_name, given_name, middle_name, birth_date, email, program, block, address, contact_number, eContact_number, eContact_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (new_student.sr_code, new_student.last_name, new_student.given_name, new_student.middle_name, new_student.birth_date, new_student.email, new_student.program, new_student.block, new_student.address, new_student.contact_number, new_student.eContact_number, new_student.eContact_name)
+
+def add_user(role, user_info):
+    if role == "student":
+        table_name = "Students"
+        fields = ["sr_code", "last_name", "given_name", "middle_name", "birth_date", "email", "program", "block", "address", "contact_number", "eContact_number", "eContact_name"]
+    else:
+        table_name = "Instructors"
+        fields = ["emp_code", "last_name", "given_name", "middle_name", "birth_date", "email", "hdl_course", "hdl_block", "address", "contact_number", "eContact_number", "eContact_name"]
+
+    query = f"INSERT INTO {table_name} ({', '.join(fields)}) VALUES ({', '.join(['%s'] * len(fields))})"
+    values = tuple(user_info[fields.index(field)] for field in fields)
 
     return query, values
 
-def add_instrcutor():
-    instructor_info = models.Instructor.collect_instructor_input()
-    new_instructor = models.Instructor(*instructor_info)
-    
-    query = "INSERT INTO Instructor (emp_code, last_name, given_name, middle_name, birth_date, email, hdl_course, hdl_block, address, contact_number, eContact_number, eContact_name) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    values = (new_instructor.emp_code, new_instructor.last_name, new_instructor.given_name, new_instructor.middle_name, new_instructor.birth_date, new_instructor.email, new_instructor.hdl_course, new_instructor.hdl_block, new_instructor.address, new_instructor.contact_number, new_instructor.eContact_number, new_instructor.eContact_name)
-    
-    return query, values
-
-def search_user_id(user_id):
-    with database.Database() as db:
-        role_query = "SELECT Role FROM users WHERE user_id = %s"
-        role_result = db.fetch_all(role_query, (user_id,))
-        return role_result
-            
 def input_data(role):
-    # Establish a connection to the database, initialization of user
+    user_info = models.Student.collect_student_input() if role == "student" else models.Instructor.collect_instructor_input()
+    
     with database.Database() as db:
-        if role == "student":
-            new_user = add_student()
-            
-        else:
-            new_user = add_instrutor()
-        
+        new_user = add_user(role, user_info)
         query, values = new_user
         
         user_id = values[0]
         email = values[5]
         password = values[0]
             
-        success = db.execute_query(query,values)
+        success = db.execute_query(query, values)
 
         # Add the user information to the 'users' table
         query = "INSERT INTO users (user_id, email, Password, Role) VALUES (%s, %s, %s, %s)"
@@ -51,8 +38,8 @@ def input_data(role):
         if success:
             print(f"User {user_id} with role {role} added successfully.")
         else:
-            print(f"Failed to add the user with role {role}.")   
-             
+            print(f"Failed to add the user with role {role}.")
+
 def delete_data(user_id):
     with database.Database() as db:
         # Determine the role of the user
@@ -63,18 +50,41 @@ def delete_data(user_id):
             return
         else:
             role = role_result[0][0]
-            
+            table_name = "Students" if role == "student" else "Instructors"
+
             # Delete the user from the respective table based on their role
-            if role == "student":
-                # Delete the student
-                db.execute_query("DELETE FROM Students WHERE sr_code = %s", (user_id,))
-            elif role == "teacher":
-                # Delete the instructor
-                db.execute_query("DELETE FROM Instructor WHERE emp_code = %s", (user_id,))
+            db.execute_query("DELETE FROM {table_name} WHERE {column_name} = %s".format(table_name=table_name, column_name='sr_code' if role == 'student' else 'emp_code'), (user_id,))
 
             # Delete the user from the 'users' table
             db.execute_query("DELETE FROM users WHERE user_id = %s", (user_id,))
             print(f"User {user_id} deleted successfully.")
+
+def search_user_id(user_id):
+    with database.Database() as db:
+        role_query = "SELECT Role FROM users WHERE user_id = %s"
+        role_result = db.fetch_all(role_query, (user_id,))
+        return role_result
+            
+
+def update_id(user_id, email, role):
+    new_user_id = input(f"Enter the {role}'s new ID: ")
+    with database.Database() as db:
+        if role == "student":
+            table_name = "Students"
+        elif role == "instructor":
+            table_name = "Instructor"
+        else:
+            print("Invalid role.")
+            return
+
+        # Update user's ID in the 'users' table
+        update_user_id_query = "UPDATE users SET user_id = %s WHERE email = %s"
+        db.execute_query(update_user_id_query, (new_user_id, email))
+
+        # Update the ID in the role's table
+        query = "UPDATE {table_name} SET {column_name} = %s WHERE email = %s".format(table_name=table_name,column_name='sr_code' if role == 'student' else 'emp_code')
+        db.execute_query(query, (new_user_id, email))
+        print(f"User ID for user {email} updated successfully.")
 
 def update_user_id():
     with database.Database() as db:
@@ -86,24 +96,29 @@ def update_user_id():
             print("Invalid credentials. Please try again.\n")
             return
 
-        # Update the user's ID in the 'users' table and their role's table
-        new_user_id = input("Enter the new ID: ")
-        update_user_id_query = "UPDATE users SET user_id = %s WHERE email = %s AND password = %s"
-        db.execute_query(update_user_id_query, (new_user_id, email, password))
+        update_id(user[0], email, user[1])
+    
+def update_field(user_id, role, field_name, choice):
+    new_value = input(f"Enter the new {field_name}: ")
+    table_name = "Students" if role == "student" else "Instructors"
+    
+    with database.Database() as db:
+        # Check that the field_name corresponds to a valid field in the table
+        valid_fields = {
+            "last_name", "given_name", "middle_name", "birth_date", "email",
+            "program", "block", "address", "contact_number", "eContact_name", "eContact_number",
+            "hdl_course", "hdl_block"
+        }
+        if field_name in valid_fields:
+            query = "UPDATE {table_name} SET {field_name} = %s WHERE {column_name} = %s".format(table_name=table_name,field_name = field_name, column_name = 'sr_code' if role == 'student' else 'emp_code')
+            db.execute_query(query, (new_value, user_id))
+            if choice == 5:
+                update_user_email_query = "UPDATE users SET email = %s WHERE user_id = %s"
+                db.execute_query(update_user_email_query, (new_value, user_id))
+            print(f"{field_name.capitalize()} updated successfully.")
+        else:
+            print(f"{field_name} is not a valid field for {table_name}.")
 
-        role = user[1]
-
-        if role == "student":
-            # Update student ID
-            update_student_id_query = "UPDATE Students SET sr_code = %s WHERE email = %s"
-            db.execute_query(update_student_id_query, (new_user_id, email))
-        elif role == "teacher":
-            # Update instructor ID
-            update_instructor_id_query = "UPDATE Instructor SET emp_code = %s WHERE email = %s"
-            db.execute_query(update_instructor_id_query, (new_user_id, email))
-
-        print(f"User ID for user {email} updated successfully.")
-        
 def update_info():
     user_id = input("Enter the ID of the user to modify: ")
     with database.Database() as db:
@@ -117,39 +132,16 @@ def update_info():
         role = role_result[0][0]
 
         if role == "student":
-            # Update student information
-            program = input("Enter Program: ")
-            block = input("Enter Block: ")
-            updated_info = models.Person.collect_person_input()
+            fields = ["last_name", "given_name", "middle_name", "birth_date", "email", "program", "block", "address", "contact_number", "eContact_name", "eContact_number"]
+        elif role == "instructor":
+            fields = ["last_name", "given_name", "middle_name", "birth_date", "email", "hdl_course", "hdl_block", "address", "contact_number", "eContact_name", "eContact_number"]
 
-            query = "UPDATE Students SET program = %s, block = %s, last_name = %s, given_name = %s, middle_name = %s, birth_date = %s, email = %s, address = %s, contact_number = %s, eContact_number = %s, eContact_name = %s WHERE sr_code = %s"
-            update_values = (program, block, *updated_info, user_id)
-            db.execute_query(query, update_values)
-
-            print(f"User {user_id} information updated successfully.")
-
-            # Print the updated information
-            info_query = "SELECT * FROM Students WHERE sr_code = %s"
-            print("Updated Student Information:")
-            print(db.fetch_all(info_query, (user_id,)))
-
-        elif role == "teacher":
-            # Update instructor information
-            hdl_course = input("Enter Handled Course: ")
-            hdl_block = input("Enter Handled Block: ")
-            updated_info = models.Person.collect_person_input()
-
-            query = "UPDATE Instructor SET hdl_course = %s, hdl_block = %s, last_name = %s, given_name = %s, middle_name = %s, birth_date = %s, email = %s, address = %s, contact_number = %s, eContact_number = %s, eContact_name = %s WHERE emp_code = %s"
-            update_values = (hdl_course, hdl_block, *updated_info, user_id)
-            db.execute_query(query, update_values)
-
-            print(f"User {user_id} information updated successfully.")
-
-            # Print the updated information
-            info_query = "SELECT * FROM Instructor WHERE emp_code = %s"
-            print("Updated Instructor Information:")
-            print(db.fetch_all(info_query, (user_id,)))
-
+        while True:
+            choice = user_interface.admin_manage_users_modify_update_info_menu_prompt(role)
+            if choice == 0:
+                break
+            elif 1 <= choice <= len(fields):
+                update_field(user_id, role, fields[choice - 1],choice)
 
 def update_password():
     user_id = input("Enter the ID of the user: ")
@@ -168,119 +160,52 @@ def update_password():
         # Print a confirmation message
         print(f"Password for user {user_id} updated successfully.")
 
-def list_students():
+def display_as_table(data, role):
+    if role == "student":
+        table = PrettyTable()
+        table.field_names = ["Student ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Program", "Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
+    else:
+        table = PrettyTable()
+        table.field_names = ["Instructor ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Handled Course", "Handled Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
+
+    for item in data:
+        table.add_row(item)
+
+    print(f"List of {role.capitalize()}s:")
+    print(table)
+
+def display_as_key_value(data, role):
+    for i, item in enumerate(data):
+        print(f"\n{role.capitalize()} {i + 1}:")
+        for j, field in enumerate(item):
+            if role == "student":
+                fields = ["Student ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Program", "Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
+            else:
+                fields = ["Instructor ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Handled Course", "Handled Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
+            print(f"  {fields[j]}: {field}")
+
+def list_students(display_format):
     with database.Database() as db:
-        # Fetch and display student information
         query = "SELECT * FROM Students"
         students = db.fetch_all(query)
 
         if students:
-            table = PrettyTable()
-            table.field_names = ["Student ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Program", "Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
-
-            for student in students:
-                table.add_row([student[0], student[1], student[2], student[3], student[4], student[5], student[6], student[7], student[8], student[9], student[10], student[11]])
-
-            print("List of Students:")
-            print(table)
+            if display_format == "table":
+                display_as_table(students, "student")
+            elif display_format == "key-value":
+                display_as_key_value(students, "student")
         else:
             print("No students found.")
 
-def list_instructors():
+def list_instructors(display_format):
     with database.Database() as db:
-        # Fetch and display instructor information
-        query = "SELECT * FROM Instructor"
+        query = "SELECT * FROM Instructors"
         instructors = db.fetch_all(query)
 
         if instructors:
-            table = PrettyTable()
-            table.field_names = ["Instructor ID", "Last Name", "Given Name", "Middle Name", "Birth Date", "Email", "Handled Course", "Handled Block", "Address", "Contact Number", "Emergency Contact Name", "Emergency Contact Number"]
-
-            for instructor in instructors:
-                table.add_row([instructor[0], instructor[1], instructor[2], instructor[3], instructor[4], instructor[5], instructor[6], instructor[7], instructor[8], instructor[9], instructor[10], instructor[11]])
-
-            print("List of Instructors:")
-            print(table)
+            if display_format == "table":
+                display_as_table(instructors, "instructor")
+            elif display_format == "key-value":
+                display_as_key_value(instructors, "instructor")
         else:
             print("No instructors found.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# """ import database
-
-# def collect_user_input(field_names):
-#     user_data = {}
-#     for field in field_names:
-#         value = input(f"Enter {field}: ")
-#         user_data[field] = value
-#     print("User data collected:", user_data)  # Debugging print
-#     return user_data
-
-# def collect_data_and_insert_to_db(table_name, field_names):
-#     user_data = collect_user_input(field_names)
-#     with database.Database() as db:
-#         try:
-#             columns = ", ".join(user_data.keys())
-#             placeholders = ", ".join(["%s"] * len(user_data))
-#             values = tuple(user_data.values())
-
-#             query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
-#             success = db.execute_query(query, values)
-
-#             if success:
-#                 print(f"Data added to the {table_name} table successfully.")
-#                 return user_data  # Return the user data
-#             else:
-#                 print(f"Failed to add data to the {table_name} table.")
-#                 return None
-#         except Exception as e:
-#             db.connection.rollback()
-#             print(f"An error occurred: {e}")
-
-# def initialize_students_interactively():
-#     student_fields = ["sr_code", "last_name", "given_name", "middle_name", "birth_date", "email", "program", "block", "address", "contact_number", "eContact_number", "eContact_name"]
-#     user_data = collect_data_and_insert_to_db("Students", student_fields)
-
-#     if user_data:
-#         user_id = user_data.get("sr_code")
-#         password = user_data.get("sr_code")  # Set the password to sr_code
-
-# def initialize_instructors_interactively():
-#     instructor_fields = ["emp_code", "last_name", "given_name", "middle_name", "birth_date", "email", "hdl_course", "hdl_block", "address", "contact_number", "eContact_number", "eContact_name"]
-#     user_data = collect_data_and_insert_to_db("Instructors", instructor_fields)
-
-#     if user_data:
-#         user_id = user_data.get("emp_code")
-#         password = user_data.get("emp_code")  # Set the password to emp_code """
