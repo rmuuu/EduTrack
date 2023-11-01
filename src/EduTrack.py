6a03de07bab33b9974fd82cbaf698f48feeade38
Aeldred Dela Cruz <21-07614@g.batstate-u.edu.ph>
2023-11-01 22:12:30 +0800

    corrected the logic for admin priv

--- src/EduTrack.py
+++ src/EduTrack.py
@@ -8,23 +8,23 @@ def main():
         print("Welcome to the EduTrack!")
         email = input("Email: ")
         password = input("Password: ")
-
-    # Admin Privilege
+        
+        user = authentication.authenticate_user(email, password)
+        
+        # Admin Privilege
         if email == "":
             if password == "":
-                admin_functions.admin_menu(user)
-
-        user = authentication.authenticate_user(email, password)
-
-        if user is None:
+                admin = admin_functions.admin_menu()
+                print("Logged out.\n")
+        elif user is None:
             print("Invalid credentials. Please try again.\n")
     
-    if user.role == "teacher":
-        print("this executed, teahcer")
-        teacher_functions.teacher_menu(user)
-    elif user.role == "student":
+    if user[1] == "student":
         print("this executed, student")
-        student_functions.student_menu(user)
+        student_functions.student_menu(user[1])
+    elif user[1] == "instructor":
+        print("this executed, instructor")
+        instructor_functions.instructor_menu(user[1])
 
 if __name__ == "__main__":
-    main()  
+    main() 
